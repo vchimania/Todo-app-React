fc3fb73e159f99480d9701682ea31c66e5a3af42
vchimania <vinichimania98@gmail.com>
2021-07-10 19:56:18 +0530

    initial  commit

---
+++ src/List.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+import "./styles.css";
+import ToDo from "./ToDo";
+import ColorPicker from "./ColorPicker.jsx";
+import colors from "./colors.js";
+
+let index = 2;
+
+export default function List() {
+  const [selectedColor, setSelectedColor] = React.useState(
+    colors[parseInt(Math.random() * colors.length, 10)]
+  );
+  const [todos, settodos] = React.useState([
+    {
+      id: 1,
+      title: "",
+      description: "",
+      color: selectedColor,
+      completed: false,
+      delete: false
+    }
+  ]);
+  const addToDo = () => {
+    const todo = [
+      {
+        id: index,
+        title: "",
+        description: "",
+        color: selectedColor,
+        completed: false,
+        delete: false
+      }
+    ];
+
+    const temp = todos.slice();
+    settodos(todo.concat(temp));
+    index++;
+    console.log(selectedColor);
+  };
+  const ondelete = (id) => {
+    setTimeout(() => {
+      const temp = todos.filter((todo) => todo.id !== id);
+      settodos(temp);
+    }, 200);
+  };
+  const oncompleted = (id) => {
+    const temp = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.completed = todo.completed ? false : true;
+      }
+      return todo;
+    });
+    settodos(temp);
+  };
+  const handleChange = (color, event) => {
+    setSelectedColor(color.hex);
+    // color = {
+    //   hex: '#333',
+    //   rgb: {
+    //     r: 51,
+    //     g: 51,
+    //     b: 51,
+    //     a: 1,
+    //   },
+    //   hsl: {
+    //     h: 0,
+    //     s: 0,
+    //     l: .20,
+    //     a: 1,
+    //   },
+    // }
+  };
+  return (
+    <div>
+        <h2> Todo App</h2>
+      <div className="selection_box">
+        <ColorPicker className="color_picker" handleChange={handleChange} />
+        <div className="addToDo" onClick={() => addToDo()}>
+          + ADD TO-DO
+        </div>
+      </div>
+      <div className="list">
+        {todos.length !== 0
+          ? todos.map((todo) => (
+              <ToDo
+                key={todo.id}
+                id={todo.id}
+                title={todo.title}
+                description={todo.description}
+                color={todo.color}
+                del={todo.delete}
+                completed={todo.completed}
+                ondelete={ondelete}
+                oncompleted={oncompleted}
+              />
+            ))
+          : "No Tasks Left"}
+      </div>
+    </div>
+  );
+}
---
+++ src/ToDo.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import "./styles.css";
+
+export default function ToDo(props) {
+  const tick_icon = "./tick-icon.png",
+    cross_icon = "./cross-icon.png",
+    delete_icon = "./bin-icon.png";
+
+  const [description, setDescription] = React.useState("");
+  const [title, setTitle] = React.useState("");
+  const [del, setDel] = React.useState(props.del);
+  const { id, completed, color, ondelete, oncompleted } = props;
+  const style = {
+    backgroundColor: color
+  };
+  return (
+    <div className={"To-Do " + color + (del ? " del" : "")} style={style}>
+      <input
+        className={"title ".concat(completed ? "done" : "")}
+        placeholder="Title"
+        value={title}
+        onChange={(event) => {
+          const str = event.target.value;
+          const temp = str.charAt(0).toUpperCase() + str.slice(1);
+          setTitle(temp);
+        }}
+      ></input>
+      <textarea
+        className={"description ".concat(completed ? "done" : "")}
+        placeholder="Description"
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+      ></textarea>
+      <br />
+      <div className="right">
+        <img
+          className="remove"
+          src={delete_icon}
+          alt="Remove"
+          width="20px"
+          title="Delete TO-DO"
+          onClick={() => {
+            setDel(true);
+            ondelete(id);
+          }}
+        ></img>
+        <img
+          className="completed"
+          src={completed ? cross_icon : tick_icon}
+          alt={completed ? "Mark Not Done" : "Mark Done"}
+          width="20px"
+          title={completed ? "Mark Not Done" : "Mark Done"}
+          onClick={() => oncompleted(id)}
+        ></img>
+      </div>
+    </div>
+  );
+}
